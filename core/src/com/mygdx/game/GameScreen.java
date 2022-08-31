77f5fe0c8ca3fb878432cb028657356f8b18c65f
Miller Peter <PMiller23>
2022-08-31 12:05:47 -0700

    First Commit

---
+++ core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,93 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class GameScreen implements Screen {
+
+    private static final float WORLD_WIDTH = 800;
+    private static final float WORLD_HEIGHT = 600;
+
+    //Object that allows us to draw all our graphics
+    private SpriteBatch spriteBatch;
+
+    //Object that allwos us to draw shapes
+    private ShapeRenderer shapeRenderer;
+
+    //Camera to view our virtual world
+    private Camera camera;
+
+    //control how the camera views the world
+    //zoom in/out? Keep everything scaled?
+    private Viewport viewport;
+    private Texture emptyTile;
+    private Texture questionTile;
+    private Texture bombTile;
+    private Texture numberTile;
+
+    //runs one time, at the very beginning
+    //all setup should happen here
+    @Override
+    public void show() {
+        camera = new OrthographicCamera(); //2D camera
+        camera.position.set(WORLD_WIDTH/2, WORLD_HEIGHT/2,0);//move the camera
+        camera.update();
+
+        //freeze my view to 800x600, no matter the window size
+        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+
+        spriteBatch = new SpriteBatch();
+
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setAutoShapeType(true); //???, I just know that this was the solution to an annoying problem
+        questionTile= new Texture("QuestionTile.png");
+    }
+
+    public void clearScreen() {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    //this method runs as fast as it can, repeatedly, constantly looped
+    @Override
+    public void render(float delta) {
+        clearScreen();
+        spriteBatch.begin();
+        spriteBatch.draw(questionTile,0,0);
+        spriteBatch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        spriteBatch.dispose();
+        shapeRenderer.dispose();
+    }
+}
