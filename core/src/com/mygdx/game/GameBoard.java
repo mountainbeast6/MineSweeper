03717abac7a02ab2e66d716d6ffe994db8a9f6c7
Miller Peter <PMiller23>
2022-09-09 13:44:11 -0700

    num tiles working

--- core/src/com/mygdx/game/GameBoard.java
+++ core/src/com/mygdx/game/GameBoard.java
@@ -13,7 +13,15 @@ public class GameBoard {
     private Texture emptyTile;
     private Texture questionTile;
     private Texture bombTile;
-    private Texture numberTile;
+    private Texture onetile;
+    private Texture twotile;
+    private Texture threetile;
+    private Texture fourtile;
+    private Texture fivetile;
+    private Texture sixtile;
+    private Texture seventile;
+    private Texture eighttile;
+
     private Texture flaggedTile;
     private int mouseX;
     private int mouseY;
@@ -24,15 +32,25 @@ public class GameBoard {
         initEmptyBoard();
         emptyTile= new Texture("BlankNull.png");
         bombTile=new Texture("Bomb.png");
+        onetile=new Texture("oneTile.png");
+        twotile=new Texture("twoTile.png");
+        threetile=new Texture("threeTile.png");
+        fourtile=new Texture("fourTile.png");
+        fivetile=new Texture("fiveTile.png");
+        sixtile=new Texture("sixTile.png");
+        seventile=new Texture("sevenTile.png");
+        eighttile=new Texture("eightTile.png");
     }
-    public boolean isValidLoc(int row, int col){
-        return row>=0&&row<board.length&&col>=0&&col<board[0].length;
+    public boolean isValidLoc(int row, int col) {
+        return row >= 0 && row < board.length &&
+                col >= 0 && col < board[0].length;
     }
+
     private void placeBombs(int rowClicked, int colClicked){
         int bombCount = 0;
         while(bombCount<99){
             int randomRow=(int)(Math.random()* board.length);
-            int randomCol=(int)(Math.random()*board.length);
+            int randomCol=(int)(Math.random()*board[0].length);
             if(randomRow!=rowClicked&&randomCol!=colClicked){
                 if(board[randomRow][randomCol]%10!=9){
                     board[randomRow][randomCol]=19;
@@ -42,12 +60,12 @@ public class GameBoard {
         }
     }
     private int[] getNeighbors(int row, int col){
-        int[]storage= new int[8];
+        int[]storage= new int[9];
         int counter = 0;
         for(int i =-1; i<2;i++){
             for(int j=-1; j<2;j++){
                 if(isValidLoc(row+i,col+j)){
-                    storage[counter]=board[row+i][row+j];
+                    storage[counter]=board[row+i][col+j];
                     counter++;
                 }
             }
@@ -64,6 +82,7 @@ public class GameBoard {
                 board[rowClicked][colClicked] = board[rowClicked][colClicked]%10;
             }
             placeBombs(rowClicked,colClicked);
+            initBoardNumbers();
             firstclick=false;
         }
         if (isValidLoc(rowClicked, colClicked)) {
@@ -76,14 +95,38 @@ public class GameBoard {
         if(cover==EMPTYTILE){
             return emptyTile;
         }
-        if (cover==FLAGGEDTILE){
+        else if (cover==FLAGGEDTILE){
             return emptyTile;
         }
-        if (cover==QUESTIONTILE){
+        else if (cover==QUESTIONTILE){
             return emptyTile;
         }
-        if (number==9){
+        else if (number==9){
             return bombTile;
+        }
+        else if(number==1){
+            return onetile;
+        }
+        else if(number==2){
+            return twotile;
+        }
+        else if(number==3){
+            return threetile;
+        }
+        else if(number==4){
+            return fourtile;
+        }
+        else if(number==5){
+            return fivetile;
+        }
+        else if(number==6){
+            return sixtile;
+        }
+        else if(number==7){
+            return seventile;
+        }
+        else if(number==8){
+            return eighttile;
         }
         return fixed;
     }
@@ -100,5 +143,24 @@ public class GameBoard {
                 board[i][j]=10;
             }
         }
+    }
+    private void initBoardNumbers(){
+        for(int i=0; i<board.length;i++){
+            for(int j=0; j<board[0].length;j++){
+                if(board[i][j]%10!=BOMB){
+                    board[i][j]=bombsAroundLoc(i,j)+10;
+                }
+            }
+        }
+    }
+    private int bombsAroundLoc(int row, int col){
+        int[]locs=getNeighbors(row,col);
+        int count = 0;
+        for(int i=0; i<locs.length;i++){
+            if(locs[i]%10==BOMB){
+                count++;
+            }
+        }
+        return count;
     }
 }
